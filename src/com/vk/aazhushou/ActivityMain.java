3a63016ca801d7caf4ea1f45c8aa97448379a21a
V-KING <firebaolong@163.com>
2015-08-04 00:46:36 +0800

    first commit

---
+++ src/com/vk/aazhushou/ActivityMain.java
@@ -0,0 +1,14 @@
+package com.vk.aazhushou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.lay_main);
+	}
+
+}
